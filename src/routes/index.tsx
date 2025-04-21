3fe4dbf338af4024a37f265c4738c5589f8212fe
Gabriel Jezewski <gabrieljezewski@MacBook-Pro-de-Gabriel.local>
2025-04-21 19:18:23 -0300

    feat: add favicon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import Login from "../pages/Login";
 import ListDragons from "../pages/ListDragons";
 import PrivateRoute from "../components/PrivateRoute";
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" />,
+  },
   {
     path: "/login",
     element: <Login />,
